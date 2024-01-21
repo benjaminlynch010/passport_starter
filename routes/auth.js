29f58769d99b00f9834374af064fb6ffaf11aca6
Ben Lynch <bdlynch7@gmail.com>
2024-01-21 16:09:41 -0600

    got enough to get started on real project

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oidc')
+const db = require("../db");
+const router = express.Router(); 
 
-router.get('/login', function(req, res, next){
-    res.render('login')
-})
+router.get("/login", function (req, res, next) {
+    res.render("login");
+});
+router.get('/login/federated/google', passport.authenticate('google'));
 
 module.exports = router;
